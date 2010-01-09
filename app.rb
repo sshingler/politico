1d223ed0fd55bc58b31c47f962f821be3683f08a
sshingler <shingler@gmail.com>
2010-01-09 16:13:09 +0000

    replacing .new with .create - timestamps and _id are respected

--- app.rb
+++ app.rb
@@ -16,8 +16,8 @@ get '/' do
 end
 
 get '/create' do
-  Party.new({:name => "Labour", :url => "http://feeds.feedburner.com/LabourPartyNews?format=xml"}).save
-  Party.new({:name => "Conservative", :url => "http://www.conservatives.com/XMLGateway/RSS/News.xml"}).save
-  Party.new({:name => "LibDem", :url => "http://www.libdems.org.uk/latest_news.aspx?view=RSS"}).save
-  Party.new({:name => "Green", :url => "http://www.greenparty.org.uk/news.rss"}).save
+  Party.create({:name => "Labour", :url => "http://feeds.feedburner.com/LabourPartyNews?format=xml"}).save
+  Party.create({:name => "Conservative", :url => "http://www.conservatives.com/XMLGateway/RSS/News.xml"}).save
+  Party.create({:name => "LibDem", :url => "http://www.libdems.org.uk/latest_news.aspx?view=RSS"}).save
+  Party.create({:name => "Green", :url => "http://www.greenparty.org.uk/news.rss"}).save
 end
