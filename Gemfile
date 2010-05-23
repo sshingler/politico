62065c78528005219804964eb4693a99a6409292
sshingler <shingler@gmail.com>
2010-05-23 18:17:16 +0100

    Removing tests, not used

--- Gemfile
+++ Gemfile
@@ -7,22 +7,3 @@ gem "mongo_mapper", "0.7.6"
 gem "haml", "3.0.4"
 gem "chowder", "0.2.4.1"
 gem "rack-flash", "0.1.1"
-
-group :development do
-  gem "ruby-debug19", "0.11.6", :require => nil
-  gem "shotgun", "0.6"
-end  
-
-group :test do
-  gem "test-unit", "1.2.3", :require => "test/unit"
-
-  gem "cucumber", "0.6.4"
-  gem "webrat", "0.7.1"
-  gem "rack-test", "0.5.3", :require => "rack/test"
-  gem "rspec", "1.3.0"
-  gem "rspec_hpricot_matchers", "1.0"
-  
-  gem "shoulda", "2.10.3"
-  gem "factory_girl", "1.2.3"
-  gem "webmock", "1.0.0"#, :require => nil
-end
--- app.rb
+++ app.rb
@@ -7,29 +7,13 @@ require "jkl"
 
 require "lib/models"
 
-config = {}
-begin
-  config = YAML::load_file('config/config.yml')
-rescue Errno::ENOENT => e
-end
-
-mongo_host = ENV['MONGO_HOST'] || config['mongo-host']
-mongo_db   = ENV['MONGO_DB']   || config['mongo-db'] 
-mongo_user = ENV['MONGO_USER'] || config['mongo-user'] 
-mongo_pass = ENV['MONGO_PASS'] || config['mongo-pass'] 
-
-MongoMapper.connection = Mongo::Connection.new(mongo_host, 27017)
-MongoMapper.database = mongo_db
-MongoMapper.database.authenticate(mongo_user, mongo_pass)
-
-key = ENV['CALAIS_KEY'] || YAML::load_file("config/keys.yml")["calais"]
-
 get '/' do
   @trends = Trend.all(:created_at.gte => 24.hours.ago)
   haml :index
 end
 
 get '/:trend/cluster' do
+  key = ENV['CALAIS_KEY'] || YAML::load_file("config/keys.yml")["calais"]
   name = CGI::unescape(params[:trend])
   @trend = Trend.find_by_name(name)
   cluster = Cluster.find_by_trend_id(@trend.id)
