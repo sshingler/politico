a2960910c8a9374d869d469472fb128e3aecc014
sshingler <shingler@gmail.com>
2010-05-23 18:24:21 +0100

    Adding specific heroku-required bundler code.

--- app.rb
+++ app.rb
@@ -7,6 +7,16 @@ require "jkl"
 
 require "lib/models"
 
+begin
+  # Require the preresolved locked set of gems.
+  require ::File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fallback on doing the resolve at runtime.
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
+
 get '/' do
   @trends = Trend.all(:created_at.gte => 24.hours.ago)
   haml :index
